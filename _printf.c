615799de518da72e182ec58f4b40c5f44a8b1129
Augusyyy <8851@holbertonstudents.com>
2024-04-01 16:24:40 +0000

    printtt

--- _printf.c
+++ _printf.c
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	va_start(args,format);
+	va_start(args, format);
 
 	while (*p)
 	{
