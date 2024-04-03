bff6f88504134cd74691c45c3941ae2480b9d1f0
Augusyyy <8851@holbertonstudents.com>
2024-04-03 00:48:45 +0000

    papopo

--- convert_c.c
+++ convert_c.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * convert_c - Converts an argument to an unsigned char and
- *             stores it to a buffer contained in a struct.
+ * convert_x - Converts an unsigned int argument to hex using abcdef
+ *             and stores it to a buffer contained in a struct.
  * @args: va_list
  * @flags: Flag modifiers.
  * @wid: A width modifier.
@@ -9,59 +9,31 @@
  * @length: length
  * Return: output length
  */
-unsigned int convert_c(va_list *args, int *flags,
+unsigned int convert_x(va_list *args, int *flags,
 		int wid, int prec, unsigned char length)
 {
-	char c;
-	unsigned int len = 0;
-
-	(void) prec;
-	(void) length;
-	c = va_arg(*args, int);
-	len += print_width(1, flags, wid);
-	len += _putchar(c);
-	len += print_neg_width(1, flags, wid);
-	return (len);
-}
-
-/**
- * convert_p - Converts the address of an argument to hex and
- *             stores it to a buffer contained in a struct.
- * @args: va_list
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: precision
- * @length: length
- * Return: output length
- */
-unsigned int convert_p(va_list *args, int *flags,
-		int wid, int prec, unsigned char length)
-{
-	unsigned long int address;
-	char *null = "(nil)";
+	unsigned long int digit;
 	unsigned int ret = 0;
 	char str[100] = {0};
 	int size = 0;
 	char *q1;
 
-	(void) length;
-	address = va_arg(*args, unsigned long int);
-	if (address == '\0')
+	digit = va_arg(*args, unsigned long int);
+	if (length == LONG)
+		digit = (unsigned long int) digit;
+	else
+		digit = (unsigned int) digit;
+	if (length == SHORT)
+		digit = (unsigned short)digit;
+	if (flags[HASH] == 1 && digit != 0)
 	{
-		while (*null)
-		{
-			ret += _putchar(*null);
-			null++;
-		}
-		return (ret);
+		ret += _putchar('0');
+		ret += _putchar('x');
 	}
-	if (!(address == 0 && prec == 0))
+	if (!(digit == 0 && prec == 0))
 	{
-		q1 = chextoa(address, str, prec, &size);
-		_putchar('0');
-		_putchar('x');
-		ret = 2;
-		ret += print_string_width(flags, wid, prec, size + 2);
+		q1 = cuitoa(digit, str, 16, prec, &size);
+		ret += print_string_width(flags, wid, size, size);
 		prec = (prec == -1) ? size : prec;
 		while (*q1 != '\0')
 		{
@@ -73,9 +45,10 @@ unsigned int convert_p(va_list *args, int *flags,
 	ret += print_neg_width(ret, flags, wid);
 	return (ret);
 }
+
 /**
- * convert_percent - Stores a percent sign to a
- *                   buffer contained in a struct.
+ * convert_X - Converts an unsigned int argument to hex using ABCDEF
+ *             and stores it to a buffer contained in a struct.
  * @args: va_list
  * @flags: Flag modifiers.
  * @wid: A width modifier.
@@ -83,18 +56,43 @@ unsigned int convert_p(va_list *args, int *flags,
  * @length: length
  * Return: output length
  */
-unsigned int convert_percent(va_list *args, int *flags,
+unsigned int convert_X(va_list *args, int *flags,
 		int wid, int prec, unsigned char length)
 {
-	char percent = '%';
+	unsigned long int digit;
 	unsigned int ret = 0;
+	char str[100] = {0};
+	int size = 0;
+	char *q1;
 
-	(void) args;
-	(void) prec;
-	(void) length;
-	ret += print_width(1, flags, wid);
-	ret += _putchar(percent);
-	ret += print_neg_width(1, flags, wid);
+	digit = va_arg(*args, unsigned long int);
+	if (length == LONG)
+		digit = (unsigned long)digit;
+	else
+		digit = (unsigned int)digit;
+	if (length == SHORT)
+		digit = (unsigned short)digit;
+	if (flags[HASH] == 1 && digit != 0)
+	{
+		ret += _putchar('0');
+		ret += _putchar('X');
+	}
+	if (!(digit == 0 && prec == 0))
+	{
+		q1 = cuitoa(digit, str, 16, prec, &size);
+		ret += print_string_width(flags, wid, size, size);
+		prec = (prec == -1) ? size : prec;
+		while (*q1 != '\0')
+		{
+			if (*q1 >= 97 && *q1 <= 102)
+				ret += _putchar(*q1 - 32);
+			else
+				ret += _putchar(*q1);
+			prec--;
+			q1++;
+		}
+	}
+	ret += print_neg_width(ret, flags, wid);
 	return (ret);
 }
 
