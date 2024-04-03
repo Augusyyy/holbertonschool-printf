effb85ad83802cc0e08998b714d94e46e69fe872
Augusyyy <8851@holbertonstudents.com>
2024-04-03 01:33:15 +0000

    gopapa

--- convert_base.c
+++ convert_base.c
@@ -1,145 +1,113 @@
-#include "main.h"
+#ifndef HOLBERTON_PRINTF_MAIN_H
+#define HOLBERTON_PRINTF_MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
 
 /**
- * citoa  -  Converts int num to string
- * @num: input int type integer number
- * @str: a character array.
- * @base: base format example 2,8,10,16
- * @prec: precision
- * @size: size of str
- * Return: a character array.
+ * struct flag_s - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
  */
-char *citoa(unsigned long int num, char *str, int base, int prec, int *size)
+typedef struct flag_s
 {
-	int i = 0;
-	bool isNegative = false;
-	int rem;
-	unsigned long int copy;
+	unsigned char flag;
+	unsigned char value;
+} flag_t;
 
-	if (num == 0)
-	{
-		str[i++] = '0';
-	}
-	if (num < 0 && base == 10)
-	{
-		isNegative = true;
-		copy = -num;
-	}
-	else
-	{
-		copy = num;
-	}
-	while (copy != 0)
-	{
-		rem = copy % base;
-		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-		copy = copy / base;
-	}
-	while (prec > i)
-	{
-		str[i++] = '0';
-	}
-	if (isNegative)
-		str[i++] = '-';
-	str[i] = '\0';
-	reverse(str, i);
-	*size += i;
-	return (str);
-}
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
+
+#define PLUS 0
+#define SPACE 1
+#define HASH 2
+#define ZERO 3
+#define NEG 4
+
 
 /**
- * cuitoa  -  Converts unsigned int num to string
- * @num: input unsigned int type integer number
- * @str: a character array.
- * @base: base format example 2,8,10,16
- * @prec: precision
- * @size: size of str
- * Return: a character array.
+ * struct buffer_s - A new type defining a buffer struct.
+ * @buffer: A pointer to a character array.
+ * @start: A pointer to the start of buffer.
+ * @len: The length of the string stored in buffer.
  */
-char *cuitoa(unsigned long int num, char *str, int base, int prec, int *size)
+typedef struct buffer_s
 {
-	int i = 0;
-	bool isNegative = false;
-	int rem = 0;
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
 
-	(void)prec;
-	if (num == 0)
-	{
-		str[i++] = '0';
-	}
-	while (num != 0)
-	{
-		rem = num % base;
-		str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-		num = num / base;
-	}
-	while (prec > i)
-	{
-		str[i++] = '0';
-	}
-	if (isNegative)
-		str[i++] = '-';
-	str[i] = '\0';
-	*size += i;
-	reverse(str, i);
-	return (str);
-}
-
-/**
- * chextoa -  Converts long int num to address
- * @addr: input long int type integer number
- * @str: a character array.
- * @prec: precision.
- * @size: str size.
- * Return: a character array.
- */
-char *chextoa(unsigned long int addr, char *str, int prec, int *size)
+typedef struct converter_s
 {
-	int i = 0, rem = 0;
+	unsigned char specifier;
+	unsigned int (*func)(va_list *, int *,
+			int, int, unsigned char);
+} converter_t;
 
-	(void) prec;
-	if (!addr)
-	{
-		str[i++] = '0';
-		str[i] = '\0';
-		(*size)++;
-		return (str);
-	}
-	while (addr)
-	{
-		rem = addr % 16;
-		if (rem < 10)
-			str[i] = '0' + rem;
-		else
-			str[i] = 'a' + (rem - 10);
-		addr /= 16;
-		i++;
-	}
-	str[i] = '\0';
-	*size += i;
-	reverse(str, i);
-	return (str);
-}
 
-/**
- * reverse -  reverse string
- * @str: a character array.
- * @length: length.
- * Return: a character array.
- */
-void reverse(char str[], int length)
-{
-	int start = 0;
-	int end = length - 1;
-	char temp;
+int _printf(const char *format, ...);
+int _putchar(char c);
 
-	while (start < end)
-	{
-		temp = str[start];
-		str[start] = str[end];
-		str[end] = temp;
-		end--;
-		start++;
-	}
-}
+void check1(long int *digit, unsigned char length);
+void check2(long int *digit, int *flags, int *ret, int *wid);
+
+char *citoa(unsigned long int num, char *str, int base, int prec, int *size);
+char *cuitoa(unsigned long int num, char *str, int base, int prec, int *size);
+char *chextoa(unsigned long int  addr, char *str, int prec, int *size);
+
+void reverse(char str[], int length);
+
+int parse_flags(const char *flag, int *ret);
+int parse_length(const char *modifier, int *length);
+int parse_width(va_list *args, const char *modifier, int *width);
+int parse_precision(va_list *args, const char *modifier, int *precision);
+unsigned int (*parse_specifiers(const char *specifier))(va_list *,
+		int *, int, int, unsigned char);
+
+unsigned int convert_c(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_s(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_di(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_b(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_u(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_o(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_X(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_x(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_percent(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_p(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_R(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_r(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+unsigned int convert_S(va_list *args, int *flags,
+		int wid, int prec, unsigned char length);
+
+unsigned int print_width(unsigned int printed,
+		int *flags, int wid);
+unsigned int print_neg_width(unsigned int printed,
+		int *flags, int wid);
+unsigned int print_string_width(int *flags, int wid,
+		int prec, int size);
+
+void free_buffer(buffer_t *output);
+buffer_t *init_buffer(void);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+
+#endif
 
